76de787639bf8cecb9ebf34ed538e2a01c77eba9
MUZAMMIL208 <202102573+Muzamil-coder949@users.noreply.github.com>
2025-05-24 23:14:11 -0700

    Home done

--- js/Section.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  fetch('./components/Section.html')
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById('main-section-content-area').innerHTML = data;
-    })
-    .catch(err => console.error('Error loading component:', err));
-});
